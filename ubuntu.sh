ff4c7be6692abb50dfc860264639d77284ced8a0
felipe endo <felipe.endo@campuscode.com.br>
2018-10-22 18:25:17 -0300

    add docker and docker compose

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get install -y python-software-properties software-properties-common python-dbus
+sudo apt-get install -y python-software-properties software-properties-common python-dbus \
+apt-transport-https ca-certificates
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
 sudo apt-get install -y silversearcher-ag \
@@ -45,6 +46,32 @@ echo
 
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
 ~/.cc_dotfiles/gnome-terminal-colors-solarized/install.sh
+
+echo "======================================================"
+echo "Installing Docker and Docker Compose."
+echo "======================================================"
+echo
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "======================================================"
+echo "Granting Docker privileges."
+echo "======================================================"
+echo
+sudo groupadd docker
+sudo usermod -aG docker $USER
 
 echo 'Change your terminal window to Run command as login shell and restart'
 echo 'You can find more information about this on' \
